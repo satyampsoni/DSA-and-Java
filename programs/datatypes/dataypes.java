5c0120b1afda7a6942cd0026cdfe620f5666697e
satyampsoni <satyampsoni@gmail.com>
2023-06-12 11:47:37 +0530

    datatypes added

---
+++ programs/datatypes/dataypes.java
@@ -0,0 +1,10 @@
+public class dataypes{
+public static void main(String[]args){
+        System.out.println("int is used to store numeric characters");
+        System.out.println("char is used to store character");
+        System.out.println(" float is used to store floating point numbers");
+        System.out.println(" double is used to store larger decimal numbers");
+        System.out.println("long is used to store larger numeric digits which is not able to store in int") ;
+        System.out.println(" boolean is used to store t values either true or false.");
+        }
+        }
